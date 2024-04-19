5ad6aa6b64328fa5ce84cf6bd2395471e2f52e29
Paola-Santanna <paolinhasj@gmail.com>
2024-04-18 21:25:03 -0300

    Add project files.

---
+++ Maior ou Menor de Idade/Program.cs
@@ -0,0 +1,17 @@
+﻿// 3.Faça um algorítmo que leia a idade de uma pessoa e informe se ela é maior ou menor de idade;
+
+int idade;
+
+Console.WriteLine("Digite a sua idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if (idade < 0 )
+{
+    Console.WriteLine("\nNão existe idade negativa");
+} else if (idade < 18 && idade > 0)
+{
+    Console.WriteLine("\nMENOR de Idade!");
+} else
+{
+    Console.WriteLine("\nMAIOR de Idade!");
+}
